9d72b483c97441c8bfef6855ece618d4802a54f0
threemonitors <0501402988@ukr.net>
2026-08-05 18:42:33 +0300

    homework3

--- lesson_03_syntax/home/_selflearning01.py
+++ lesson_03_syntax/home/_selflearning01.py
@@ -9,34 +9,34 @@ print("Створіть змінні: student_name, student_age, is_enrolled, gp
 print("Приклад: student_name = 'Іван Петров'")
 
 # Ваш код тут:
-student_name = ""  # Замініть на ваше ім'я
-student_age = 0    # Замініть на ваш вік
-is_enrolled = False # Замініть на True якщо навчаєтеся
-gpa_score = 0.0    # Замініть на ваш середній бал
+student_name = "Tetiana"  # Замініть на ваше ім'я
+student_age = 46          # Замініть на ваш вік
+is_enrolled = True        # Замініть на True якщо навчаєтеся
+gpa_score = 100.0         # Замініть на ваш середній бал
 
 # ЗАВДАННЯ 2: Арифметичні операції
 print("\nЗавдання 2: Виконайте арифметичні операції")
 print("Обчисліть результати операцій з числами 25 та 4")
 
 # Ваш код тут:
-addition_result = 0      # 25 + 4
-subtraction_result = 0   # 25 - 4
-multiplication_result = 0 # 25 * 4
-division_result = 0      # 25 / 4
-floor_division_result = 0 # 25 // 4
-modulo_result = 0        # 25 % 4
-power_result = 0         # 25 ** 4
+addition_result = 25 + 4        # 25 + 4
+subtraction_result = 25 - 4     # 25 - 4
+multiplication_result = 25 * 4  # 25 * 4
+division_result = 25 / 4        # 25 / 4
+floor_division_result = 25 // 4 # 25 // 4
+modulo_result = 25 % 4          # 25 % 4
+power_result = 25 ** 4          # 25 ** 4
 
 # ЗАВДАННЯ 3: Робота з рядками
 print("\nЗавдання 3: Обробіть рядок 'Python Programming Language'")
 text = "Python Programming Language"
 
 # Ваш код тут:
-first_char = ""          # Перший символ
-last_char = ""           # Останній символ
-text_length = 0          # Довжина рядка
-first_word = ""          # Перше слово (індекси 0-5)
-last_word = ""           # Останнє слово (індекси 19-26)
+first_char = text[0]           # Перший символ
+last_char = text[-1]           # Останній символ
+text_length = len(text)        # Довжина рядка
+first_word = text[0:6]         # Перше слово (індекси 0-5)
+last_word = text[19:27]        # Останнє слово (індекси 19-26)
 
 # ЗАВДАННЯ 4: Форматування рядків
 print("\nЗавдання 4: Створіть форматовані рядки")
@@ -45,9 +45,9 @@ age = 22
 height = 1.65
 
 # Ваш код тут (використайте f-strings):
-greeting = ""            # "Привіт, мене звати Марія"
-age_info = ""            # "Мені 22 роки"
-height_info = ""         # "Мій зріст 1.65 м"
+greeting = f"Привіт, мене звати {name}"  # "Привіт, мене звати Марія"
+age_info = f"Мені {age} роки"            # "Мені 22 роки"
+height_info = f"Мій зріст {height} м"    # "Мій зріст 1.65 м"
 
 # ЗАВДАННЯ 5: Конвертація типів
 print("\nЗавдання 5: Конвертуйте типи даних")
@@ -56,9 +56,9 @@ str_float = "45.67"
 number = 89
 
 # Ваш код тут:
-converted_int = 0        # str_number у int
-converted_float = 0.0    # str_float у float
-converted_str = ""       # number у str
+converted_int = int(str_number)       # str_number у int
+converted_float = float(str_float)    # str_float у float
+converted_str = str(number)           # number у str
 
 # ЗАВДАННЯ 6: Математичні обчислення
 print("\nЗавдання 6: Обчисліть площу та периметр прямокутника")
@@ -66,28 +66,28 @@ width = 12.5
 height = 8.3
 
 # Ваш код тут:
-rectangle_area = 0.0     # width * height
-rectangle_perimeter = 0.0 # 2 * (width + height)
+rectangle_area = width * height            # width * height
+rectangle_perimeter = 2 * (width + height) # 2 * (width + height)
 
 # ЗАВДАННЯ 7: Робота з індексами
 print("\nЗавдання 7: Витягніть символи з рядка за індексами")
 sample_text = "Programming"
 
 # Ваш код тут:
-char_at_0 = ""           # Символ на позиції 0
-char_at_5 = ""           # Символ на позиції 5
-char_at_minus_1 = ""     # Останній символ (індекс -1)
-char_at_minus_3 = ""     # Третій з кінця (індекс -3)
+char_at_0 = sample_text[0]           # Символ на позиції 0
+char_at_5 = sample_text[5]           # Символ на позиції 5
+char_at_minus_1 = sample_text[-1]    # Останній символ (індекс -1)
+char_at_minus_3 = sample_text[-3]    # Третій з кінця (індекс -3)
 
 # ЗАВДАННЯ 8: Зрізи рядків
 print("\nЗавдання 8: Створіть зрізи рядка")
 full_text = "Hello World Python"
 
 # Ваш код тут:
-first_five = ""          # Перші 5 символів
-middle_part = ""         # Символи з 6 по 10 включно
-last_six = ""            # Останні 6 символів
-every_second = ""        # Кожен другий символ
+first_five = full_text[0:5]          # Перші 5 символів
+middle_part = full_text[6:11]        # Символи з 6 по 10 включно
+last_six = full_text[-6:]            # Останні 6 символів
+every_second = full_text[::2]        # Кожен другий символ
 
 # ЗАВДАННЯ 9: Логічні операції
 print("\nЗавдання 9: Виконайте логічні операції")
@@ -97,9 +97,9 @@ c = True
 
 # Ваш код тут:
 and_result = False       # a and b
-or_result = False        # a or b
-not_result = True        # not a
-complex_result = False   # (a and c) or (not b)
+or_result = True        # a or b
+not_result = False        # not a
+complex_result = True   # (a and c) or (not b)
 
 # ЗАВДАННЯ 10: Складні обчислення
 print("\nЗавдання 10: Обчисліть складний вираз")
@@ -108,9 +108,9 @@ y = 3
 z = 2
 
 # Ваш код тут:
-result1 = 0              # (x + y) * z
-result2 = 0              # x ** y - z
-result3 = 0.0            # (x / y) + (z * 2)
-result4 = 0              # x % y + z ** 2
+result1 = (x + y) * z             # (x + y) * z
+result2 = x ** y - z              # x ** y - z
+result3 = (x / y) + (z * 2)       # (x / y) + (z * 2)
+result4 = x % y + z ** 2          # x % y + z ** 2
 
 print("\n=== ЗАКІНЧІТЬ ВИКОНАННЯ ЗАВДАНЬ І ЗАПУСТІТЬ ТЕСТИ ===")
